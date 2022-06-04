7fdeec03acbdd7c0042ce75aef70c9ce0db883d2
dahalh <dahalh@outlook.com>
2022-06-04 15:21:50 +1000

    frontend initialization done

---
+++ frontend-not-to-do/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      Hello world
+      <Button variant="primary">
+        Primary
+        <i class="fa-thin fa-0"></i>
+      </Button>
+    </div>
+  );
+}
+
+export default App;
